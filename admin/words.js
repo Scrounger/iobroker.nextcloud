3a861375f7ba11b03f9568346118cfe3ce6c9149
Scrounger <scrounger@gmx.net>
2020-04-12 11:57:48 +0200

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,113 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+	'nextcloud adapter settings': {
+		'en': 'Adapter settings for nextcloud',
+		'de': 'Adaptereinstellungen für nextcloud',
+		'ru': 'Настройки адаптера для nextcloud',
+		'pt': 'Configurações do adaptador para nextcloud',
+		'nl': 'Adapterinstellingen voor nextcloud',
+		'fr': "Paramètres d'adaptateur pour nextcloud",
+		'it': "Impostazioni dell'adattatore per nextcloud",
+		'es': 'Ajustes del adaptador para nextcloud',
+		'pl': 'Ustawienia adaptera dla nextcloud',
+		'zh-cn': 'nextcloud的适配器设置'
+	},
+	'option1': {
+		'en': 'option1',
+		'de': 'Option 1',
+		'ru': 'Опция 1',
+		'pt': 'Opção 1',
+		'nl': 'Optie 1',
+		'fr': 'Option 1',
+		'it': 'opzione 1',
+		'es': 'Opción 1',
+		'pl': 'opcja 1',
+		'zh-cn': '选项1'
+	},
+	'option2': {
+		'en': 'option2',
+		'de': 'Option 2',
+		'ru': 'option2',
+		'pt': 'opção 2',
+		'nl': 'Optie 2',
+		'fr': 'Option 2',
+		'it': 'opzione 2',
+		'es': 'opcion 2',
+		'pl': 'Opcja 2',
+		'zh-cn': '选项2'
+	},
+	'nextcloudIgnoreCertificateErrors': {
+		"en": "ignore Certificate Errors",
+		"de": "Zertifikatfehler ignorieren",
+		"ru": "игнорировать ошибки сертификата",
+		"pt": "ignorar erros de certificado",
+		"nl": "Certificaatfouten negeren",
+		"fr": "ignorer les erreurs de certificat",
+		"it": "ignora errori di certificato",
+		"es": "ignorar errores de certificado",
+		"pl": "zignoruj błędy certyfikatu",
+		"zh-cn": "忽略证书错误"
+	},
+	'nextcloudUrl': {
+		"en": "Nextcloud url",
+		"de": "Nextcloud URL",
+		"ru": "URL следующего облака",
+		"pt": "URL do Nextcloud",
+		"nl": "Nextcloud-URL",
+		"fr": "URL de Nextcloud",
+		"it": "Nextcloud url",
+		"es": "URL de Nextcloud",
+		"pl": "Adres URL Nextcloud",
+		"zh-cn": "Nextcloud网址"
+	},
+	"nextcloudUserName": {
+		"en": "name of user",
+		"de": "Name des Benutzers",
+		"ru": "имя пользователя",
+		"pt": "nome do usuário",
+		"nl": "naam van de gebruiker",
+		"fr": "nom d'utilisateur",
+		"it": "nome dell'utente",
+		"es": "nombre del usuario",
+		"pl": "nazwa użytkownika",
+		"zh-cn": "用户名"
+	},
+	"nextcloudUserPassword": {
+		"en": "password of user",
+		"de": "Passwort des Benutzers",
+		"ru": "пароль пользователя",
+		"pt": "senha do usuário",
+		"nl": "wachtwoord van de gebruiker",
+		"fr": "mot de passe de l'utilisateur",
+		"it": "password dell'utente",
+		"es": "contraseña del usuario",
+		"pl": "hasło użytkownika",
+		"zh-cn": "用户密码"
+	},
+	"nextcloudPollingInterval": {
+		"en": "polling interval [in seconds]",
+		"de": "Abfrageintervall [in Sekunden]",
+		"ru": "интервал опроса [в секундах]",
+		"pt": "intervalo de pesquisa [em segundos]",
+		"nl": "polling-interval [in seconden]",
+		"fr": "intervalle d'interrogation [en secondes]",
+		"it": "intervallo di polling [in secondi]",
+		"es": "intervalo de sondeo [en segundos]",
+		"pl": "interwał odpytywania [w sekundach]",
+		"zh-cn": "轮询间隔[秒]"
+	},
+	"donation": {
+		"en": "You like the adapter? Then support the adapter developer (Scrounger) with a small donation.",
+		"de": "Dir gefällt der Adapter? Dann unterstütze den Adapterentwickler (Scrounger) mit einer kleinen Spende.",
+		"ru": "Вам нравится адаптер? Затем поддержите разработчика адаптера (Scrounger) с небольшим пожертвованием.",
+		"pt": "Você gosta do adaptador? Depois, ofereça suporte ao desenvolvedor do adaptador (Scrounger) com uma pequena doação.",
+		"nl": "Vind je de adapter leuk? Ondersteun vervolgens de adapterontwikkelaar (Scrounger) met een kleine donatie.",
+		"fr": "Vous aimez l'adaptateur? Soutenez ensuite le développeur de l'adaptateur (Scrounger) avec un petit don.",
+		"it": "Ti piace l'adattatore? Quindi supportare lo sviluppatore dell'adattatore (Scrounger) con una piccola donazione.",
+		"es": "¿Te gusta el adaptador? Luego, apoye al desarrollador del adaptador (Scrounger) con una pequeña donación.",
+		"pl": "Podoba ci się adapter? Następnie wesprzyj programistę adaptera (Scrounger) niewielką darowizną.",
+		"zh-cn": "您喜欢适配器吗？然后以少量捐款支持适配器开发人员（Scrounger）。"
+	}
+};
---
+++ main.js
@@ -0,0 +1,254 @@
+'use strict';
+
+/*
+ * Created with @iobroker/create-adapter v1.23.0
+ */
+
+// The adapter-core module gives you access to the core ioBroker functions
+// you need to create an adapter
+const utils = require('@iobroker/adapter-core');
+
+
+// Load your modules here, e.g.:
+const api = require('nextcloud-node-client');
+
+let pass = "";
+
+
+
+
+class Nextcloud extends utils.Adapter {
+
+	/**
+	 * @param {Partial<ioBroker.AdapterOptions>} [options={}]
+	 */
+	constructor(options) {
+		super({
+			...options,
+			name: 'nextcloud',
+		});
+		this.on('ready', this.onReady.bind(this));
+		this.on('objectChange', this.onObjectChange.bind(this));
+		this.on('stateChange', this.onStateChange.bind(this));
+		// this.on('message', this.onMessage.bind(this));
+		this.on('unload', this.onUnload.bind(this));
+	}
+
+	/**
+	 * Is called when databases are connected and adapter received configuration.
+	 */
+	async onReady() {
+		try {
+			// Check if credentials are not empty and decrypt stored password
+			if (await this.getSettings()) {
+				await this.checkConnection();
+				// this.log.info(JSON.stringify(sys.nextcloud.system));
+				// this.log.info(JSON.stringify(sys.nextcloud.storage));
+				// this.log.info(JSON.stringify(sys.nextcloud.shares));
+
+				// this.log.info(JSON.stringify(sys.server));
+				// this.log.info(JSON.stringify(sys.activeUsers));
+
+			} else {
+				this.log.error("*** Adapter deactivated, credentials missing in Adaptper Settings !!!  ***");
+				this.setForeignState("system.adapter." + this.namespace + ".alive", false);
+			}
+
+			// // Initialize your adapter here
+
+			// // The adapters config (in the instance object everything under the attribute "native") is accessible via
+			// // this.config:
+			// this.log.info('config option1: ' + this.config.option1);
+			// this.log.info('config option2: ' + this.config.option2);
+
+			// /*
+			// For every state in the system there has to be also an object of type state
+			// Here a simple template for a boolean variable named "testVariable"
+			// Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
+			// */
+			// await this.setObjectAsync('testVariable', {
+			// 	type: 'state',
+			// 	common: {
+			// 		name: 'testVariable',
+			// 		type: 'boolean',
+			// 		role: 'indicator',
+			// 		read: true,
+			// 		write: true,
+			// 	},
+			// 	native: {},
+			// });
+
+			// // in this template all states changes inside the adapters namespace are subscribed
+			// this.subscribeStates('*');
+
+			// /*
+			// setState examples
+			// you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
+			// */
+			// // the variable testVariable is set to true as command (ack=false)
+			// await this.setStateAsync('testVariable', true);
+
+			// // same thing, but the value is flagged "ack"
+			// // ack should be always set to true if the value is received from or acknowledged from the target system
+			// await this.setStateAsync('testVariable', { val: true, ack: true });
+
+			// // same thing, but the state is deleted after 30s (getState will return null afterwards)
+			// await this.setStateAsync('testVariable', { val: true, ack: true, expire: 30 });
+
+			// // examples for the checkPassword/checkGroup functions
+			// let result = await this.checkPasswordAsync('admin', 'iobroker');
+			// this.log.info('check user admin pw iobroker: ' + result);
+
+			// result = await this.checkGroupAsync('admin', 'admin');
+			// this.log.info('check group user admin group admin: ' + result);
+
+		} catch (ex) {
+			this.log.error(`error: ${ex.message}, stack: ${ex.stack}`);
+		}
+	}
+
+	/**
+	 * Function to decrypt passwords
+	 * @param {string | { charCodeAt: (arg0: number) => number; }[]} key
+	 * @param {string} value
+	 */
+	decrypt(key, value) {
+		let result = "";
+		for (let i = 0; i < value.length; ++i) {
+			result += String.fromCharCode(key[i % key.length].charCodeAt(0) ^ value.charCodeAt(i));
+		}
+		this.log.debug("client_secret decrypt ready");
+		return result;
+	}
+
+	/**
+	 * Is called when adapter shuts down - callback has to be called under any circumstances!
+	 * @param {() => void} callback
+	 */
+	onUnload(callback) {
+		try {
+			this.log.info('cleaned everything up...');
+			callback();
+		} catch (e) {
+			callback();
+		}
+	}
+
+	/**
+	 * Is called if a subscribed object changes
+	 * @param {string} id
+	 * @param {ioBroker.Object | null | undefined} obj
+	 */
+	onObjectChange(id, obj) {
+		if (obj) {
+			// The object was changed
+			this.log.info(`object ${id} changed: ${JSON.stringify(obj)}`);
+		} else {
+			// The object was deleted
+			this.log.info(`object ${id} deleted`);
+		}
+	}
+
+	/**
+	 * Is called if a subscribed state changes
+	 * @param {string} id
+	 * @param {ioBroker.State | null | undefined} state
+	 */
+	onStateChange(id, state) {
+		if (state) {
+			// The state was changed
+			this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
+		} else {
+			// The state was deleted
+			this.log.info(`state ${id} deleted`);
+		}
+	}
+
+	async getSettings() {
+		if (this.config.nextcloudUrl !== "") {
+			process.env.NEXTCLOUD_URL = `${this.config.nextcloudUrl}/remote.php/webdav`;
+		} else {
+			this.log.warn(`no url defined in  Adaptper Settings!`);
+		}
+
+		if (this.config.nextcloudUserName !== "") {
+			process.env.NEXTCLOUD_USERNAME = this.config.nextcloudUserName;
+		} else {
+			this.log.warn(`no user name defined in  Adaptper Settings!`);
+		}
+
+		if (this.config.nextcloudUserPassword !== "") {
+			let obj = await this.getForeignObjectAsync('system.config');
+
+			if (obj && obj.native && obj.native.secret) {
+				//noinspection JSUnresolvedVariable
+				pass = this.decrypt(obj.native.secret, this.config.nextcloudUserPassword);
+			} else {
+				//noinspection JSUnresolvedVariable
+				pass = this.decrypt("Zgfr56gFe87jJOM", this.config.nextcloudUserPassword);
+			}
+
+			process.env.NEXTCLOUD_PASSWORD = pass;
+		} else {
+			this.log.warn(`no user password defined in  Adaptper Settings!`);
+		}
+
+		if (this.config.nextcloudUrl !== "" && this.config.nextcloudUserName !== "" && pass !== "") {
+			if (this.config.nextcloudIgnoreCertificateErrors) {
+				// ignore Certificate Error
+				process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = "0";
+			}
+
+			return true;
+		}
+
+		return false;
+	}
+
+	async checkConnection() {
+		try {
+			const client = new api.Client();
+			const systemInfos = await client.getSystemInfo();
+
+			await this.setStateAsync('info.connection', true, true);
+
+			return { isConnected: true, client: client, systemInfos: systemInfos }
+
+		} catch (ex) {
+			await this.setStateAsync('info.connection', false, true);
+			this.log.error(`[checkConnection] error: ${ex.message}, stack: ${ex.stack}`);
+
+			return { isConnected: false }
+		}
+	}
+
+	// /**
+	//  * Some message was sent to this instance over message box. Used by email, pushover, text2speech, ...
+	//  * Using this method requires "common.message" property to be set to true in io-package.json
+	//  * @param {ioBroker.Message} obj
+	//  */
+	// onMessage(obj) {
+	// 	if (typeof obj === 'object' && obj.message) {
+	// 		if (obj.command === 'send') {
+	// 			// e.g. send email or pushover or whatever
+	// 			this.log.info('send command');
+
+	// 			// Send response in callback if required
+	// 			if (obj.callback) this.sendTo(obj.from, obj.command, 'Message received', obj.callback);
+	// 		}
+	// 	}
+	// }
+
+}
+
+// @ts-ignore parent is a valid property on module
+if (module.parent) {
+	// Export the constructor in compact mode
+	/**
+	 * @param {Partial<ioBroker.AdapterOptions>} [options={}]
+	 */
+	module.exports = (options) => new Nextcloud(options);
+} else {
+	// otherwise start the instance directly
+	new Nextcloud();
+}
